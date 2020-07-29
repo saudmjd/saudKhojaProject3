ae0c591325d9343fd23a96324be2b26adff939b2
Saud M. Khoja <65879129+saudmjd@users.noreply.github.com>
2020-07-29 17:48:03 -0400

    simplified syntax update
    I noticed I didn't push my last commit or two correctly.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
-let $image = $('<img>');
+let $img = $('<img>');
 let $overlay = $('<div id="img-overlay"></div>');
-let $close = $('<i class="fas fa-times"></i>');
+let $closeButton = $('<i class="fas fa-times"></i>');
 
 // HTML stuff
 $('section').append($overlay);   // Add overlay to DOM
@@ -9,16 +9,16 @@ $overlay.fadeOut();   // Hide overlay
 // When user clicks on image
 $('.lightbox img').click(function(){
 	imageUrl = $(this).attr('src');   // Display img src onclick
-
-	$overlay.append($image);   // Add img to overlay
-	$overlay.append($close);   // Add close button to overlay
-	$image.attr('src', imageUrl);   // Add img attribute
+	
+	$overlay.append($img);   // Add img to overlay
+	$overlay.append($closeButton);   // Add close button to overlay
+	$img.attr('src', imageUrl);   // Add img attribute
 	$overlay.fadeIn('2000');   // Show overlay
-	$image.fadeIn('2000');   // Show img
-	$close.show();   // Close overlay
+  	$img.fadeIn('2000');   // Show img
+  	$closeButton.show();   // Close overlay
 });
 
 // Close overlay when X clicked
-$close.click( function() {
+$closeButton.click( function() {
 	$overlay.fadeOut('2000');
-} );
+});
